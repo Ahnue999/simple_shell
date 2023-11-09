43c1d04a06bf192ffd3144afbb571666b763a97d
Ahnue999 <ahnue200572@gmail.com>
2023-11-08 20:41:56 -0800

    fixings

--- builtin_env.c
+++ builtin_env.c
@@ -2,17 +2,21 @@
 
 /**
  * builtin_env - prints the process environment.
- * data: shell data.
+ * @data: shell data.
  *
- * Return:
+ * Return: 0 when success.
  */
 int builtin_env(shdata_t *data)
 {
-	int i = 0;
+	int i, j;
 
+	i = 0;
 	while(data->sh_env[i])
 	{
-		printf("%s", sh_env[i]);
+		for (j = 0; sh_env[i][j] != '\0'; j++)
+			;
+		write(STDOUT_FILENO, sh_env[i], j + 1);
+		write(STDOUT_FILENO, '\n', 1);
 		i++;
 	}
 }
